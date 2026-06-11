19f4b3ccfc8640de0cc336f282b4873c80c2f69c
letsrankup <zaheerrind043@gmail.com>
2026-06-11 19:14:28 +0500

    Create utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,20 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatCurrency(amount: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+  }).format(amount)
+}
+
+export function truncateText(text: string, length: number): string {
+  if (text.length <= length) return text
+  return text.slice(0, length) + '...'
+}
+
