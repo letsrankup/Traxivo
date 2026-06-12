6975502dc2ce7e1e5507bb9539cc92ea6b0d0e18
letsrankup <zaheerrind043@gmail.com>
2026-06-12 17:13:41 +0500

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    // 1. Tailwind CSS engine ko invoke karta hai
+    tailwindcss: {},
+    
+    // 2. Browser compatibility ke liye vendor prefixes (jaise -webkit-, -moz-) automatically add karta hai
+    autoprefixer: {},
+  },
+}
+
